a3d08c788d0d580e77ae899e5ee2ecb66f68a239
Сергей <56985915+RickGrin@users.noreply.github.com>
2020-06-15 23:46:00 +0700

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,65 +1,122 @@
 ﻿#include <iostream>
+
+#include <string.h>
+
 #include <fstream>
-#include <vector>
-#include <string>
-#include <cstdlib>
+
+#include <stdio.h>
+
+#include <cctype>
 
 using namespace std;
 
-ifstream fin;
-ofstream fout;
-
-struct name
-{
-    string First_Name, Second_Name, Third_Name;
-    friend ostream& operator<<(ostream& stream, name& name_obj)
-    {
-        stream << name_obj.First_Name << " " << name_obj.Second_Name << " " << name_obj.Third_Name << " ";
-        return stream;
-    }
-    friend istream& operator>>(istream& stream, name& name_obj)
-    {
-        stream >> name_obj.First_Name >> name_obj.Second_Name >> name_obj.Third_Name;
-        return stream;
-    }
-};
-
-struct book
-{
-    int ID;
-    name author;
-    string title;
-    int price;
-    friend ostream& operator<<(ostream& stream, book& book_obj)
-    {
-        stream << "Индивидуальный номер - " << book_obj.ID << ", автор - " << book_obj.author << ", название " << book_obj.title << ", цена - " << book_obj.price;
-        return stream;
-    }
-    friend istream& operator>>(istream& stream, book& book_obj)
-    {
-        stream >> book_obj.ID >> book_obj.author >> book_obj.title >> book_obj.price;
-        return stream;
-    }
-};
-
-void start()
-{
-    vector<book> books;
-
-    fin.open("text.txt");
-
-    while (!fin.eof())
-    {
-        books.push_back(book());
-        fin >> books.back();
-        cout << books.back() << endl;
-    }
-}
-
 int main()
-{
-    setlocale(LC_ALL, "Russian");
-    start();
 
-    return 0;
+{
+
+	setlocale(LC_ALL, "");
+
+	ifstream fin("K.txt");
+
+	int n = 0, i = 0, j = 0; int choose;
+
+	char exit[50];
+
+	struct k
+
+	{
+
+		int v; //Чит билет
+
+		int k;// Цена
+
+		char s[50];//Н книги
+
+		char n[50];//ФИО
+
+	};
+
+	while (exit[0] != '!')
+
+	{
+
+		n++;
+
+		fin.getline(exit, 50);
+
+	}
+
+	n--;
+
+	k* arr_k = new k[n];
+
+	fin.seekg(0, ios_base::beg);
+
+	for (i = 0; i < n; i++)
+
+	{
+
+		fin >> arr_k[i].v;
+
+		
+
+		fin >> arr_k[i].s;
+
+		fin >> arr_k[i].k;
+
+		fin.getline(arr_k[i].n, 50);
+
+		cout << "Номер читательского билета: " << arr_k[i].v << endl;
+
+	
+
+		cout << "Название книги: " << arr_k[i].s << endl;
+
+		cout << "ФИО автора: " << arr_k[i].n << endl;
+
+		cout << "стоимость книги в рублях: " << arr_k[i].k << endl << endl;
+
+	}
+
+	cout << "Введите номер задания-"; cin >> choose;
+
+	
+
+	if (choose == 2) 
+	{
+
+		for (i = 65; i < 91; i++)
+
+		{
+
+			for (j = 0; j < n; j++)
+
+			{
+
+				if ((arr_k[j].k > 100) && (arr_k[j].k <1000))
+
+				{
+
+					cout << "Номер читательского билета: " << arr_k[j].v << endl;
+
+						cout << "Название книги: " << arr_k[j].s << endl;
+
+						cout << "ФИО автора: " << arr_k[j].n << endl;
+
+						cout << "стоимость книги в рублях:" << arr_k[j].k << endl << endl;
+
+						return 0;
+				}
+			}
+
+			
+
+		}
+
+		system("pause");
+
+		return 0;
+
+	}
+
 }
